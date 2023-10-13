0e7c211651da6597aa93e2bf9599cd7bf9fcc19d
poravoz <116353344+poravoz@users.noreply.github.com>
2023-10-13 18:56:38 +0300

    13.10.2023

--- src/front-end/assets/js/edit.script.js
+++ src/front-end/assets/js/edit.script.js
@@ -6,6 +6,23 @@ window.onload = function() {
         const description = document.getElementById('description').value;
         const price = document.getElementById('price').value;
 
+        if (name.length < 3) {
+            alert('The name must contain at least 3 characters.');
+            return;
+        } else if (!/\d/.test(name)) {
+            alert('The name must contain at least one number.');
+            return;
+        } else if (description.length < 10) {
+            alert('The description must contain at least 10 characters.');
+            return;
+        } else  if (!/^\d+$/.test(price)) {
+            alert('The price must contain only numbers.');
+            return;
+        } else if(price < 100 || price > 150000) {
+            alert('The price should be between 100 and 150,000 and contain only numbers.');
+            return;
+        }
+
         try {
             const response = await fetch('http://localhost:3000/phone/', {
                 method: 'POST',
